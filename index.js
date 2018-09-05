54c45ba00eabb679d9812c9eb48a98f4c2790052
helen.marder@wework.com <helen.marder@wework.com>
2018-09-05 02:54:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const expect = require('expect')
-const fs = require('fs')
-const jsdom = require('jsdom')
-const path = require('path')
-
+console.log("YOU GOT THIS!")
 console.error("HaLP!")
-console.log("You got this!")
 console.warning("DO NOT PASS!")
